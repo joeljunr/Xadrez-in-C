06edba8cc45f8c1de649aaa8f55c8507cea9fec4
Joel Lopes <joeljunio59@gmail.com>
2025-05-06 18:58:04 +0000

    Adicionando a peça Cavalo e adicionando a lógica de seus movimentos

--- movxadrez.c
+++ movxadrez.c
@@ -4,7 +4,9 @@ int main() {
     
     int torre = 0;
     int bispo;
-    int rainha;
+    int rainha = 0;
+    int cavalo = 1;
+
     printf("*** Vamos movimentar seu BISPO! *** \n");
     // Aqui fazemos os movimentos requeridos do BISPO.
     // Utilizamos o for pra fazer os movimentos diagonais
@@ -34,5 +36,20 @@ int main() {
     }
     printf("\n");
     printf("\n");
+
+    printf("*** Vamos movimentar seu CAVALO! *** \n");
+    // Aqui fazemos os movimentos requeridos do cavalo,
+    // A atividade pediu dois movimentos pra baixo e uma pra esquerda.
+    while(cavalo --){
+        for (int i = 0; i < 2; i++){
+            printf("Baixo... \n");
+            // Imprime o movimento "Baixo" duas vezes, visto que a movimentação de "I" começa em 0. 
+        }
+        printf("Esquerda... \n");
+        // Imprime o movimento "Esquerda" uma vez, visto que a variavel "Cavalo" começa em 1 e é decrescente.
+    }
+
+
+
     return 0;
 }
